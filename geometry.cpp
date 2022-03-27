026e2cf339f0337cfaedb98890d797fefc02c2a4
AvinashKartik <66268505+AvinashKartik@users.noreply.github.com>
2022-03-27 23:59:04 +0530

    few more chanes to be made

--- geometry.cpp
+++ geometry.cpp
@@ -5,34 +5,6 @@
 
 using namespace std;
 
-#define  GODSPEED       ios:: sync_with_stdio(0);cin.tie(0);cout.tie(0);cout<<fixed;cout<<setprecision(15);
-#define  f              first                 
-#define  s              second
-#define  newl           cout<<"\n";
-#define  pb             push_back  
-#define  mset(a,x)      memset(a,x,sizeof(a))
-#define  debv(a)        for(auto it: a)cout<<it<<" ";newl;
-#define  deb1(a)        cout<<a<<"\n";
-#define  deb2(a,b)      cout<<a<<" "<<b<<"\n";
-#define  deb3(a,b,c)    cout<<a<<" "<<b<<" "<<c<<"\n";
-#define  deb4(a,b,c,d)  cout<<a<<" "<<b<<" "<<c<<" "<<d<<"\n";
-#define  uniq(a)        a.resize(unique(a.begin(), a.end()) - a.begin());
-#define  all(a)         a.begin(),a.end()           
- 
-typedef  long long             ll;
-typedef  long double           ld;
-typedef  pair<ll,ll>           pll;
-typedef  vector<ll>            vll;
-typedef  vector<pll>           vpll;
-
-const int  N     =  5e5+5;
-const int  mod   =  1e9+7;
-const ll   INF   =  0x7f7f7f7f7f7f7f7f;
-const int  INFi  =  0x7f7f7f7f;
-const int  LEVEL =  log2(N)+1;
-
-ll n;
-
 struct pt{
     long long x, y;
     pt(){}
@@ -54,7 +26,7 @@ bool onSegment(pt p, pt q, pt r){
 } 
 
 bool colinear(pt p, pt q, pt r){ 
-	ll val = (q.y - p.y) * (r.x - q.x) - (q.x - p.x) * (r.y - q.y); 
+	long long val = (q.y - p.y) * (r.x - q.x) - (q.x - p.x) * (r.y - q.y); 
 	return (val == 0);
 }
 
@@ -65,7 +37,7 @@ bool InTriangle(pt a, pt b, pt c, pt point){
 }
 
 bool InPolygon(vector<pt> q, vector<pt> p){
-	int ok = 0;
+	bool ok = 0;
 	for(auto it : p){
 		for(int i = 0; i < q.size(); i++){
 			if(colinear(q[i], q[(i+1)%q.size()], it)){
@@ -82,27 +54,5 @@ bool InPolygon(vector<pt> q, vector<pt> p){
 	return ok;
 }
 
-
-void solve(){
-	cin >> n;
-	vector <pt> poly[n+1];
-	for(int i = 1; i <= n; i++){
-		int vi; cin >> vi;
-		for(int j = 1; j <= vi; j++){
-			ll x, y; cin >> x >> y;
-			poly[i].pb({x, y});
-		}
-	}
-}
-
 int main(){
-	GODSPEED;
-	int test = 1;
-	cin >> test;
-	for(int i = 1; i <= test; i++){
-		solve();
-	}
-	#ifndef ONLINE_JUDGE
-	cout<<"\nTime Elapsed: " << 1.0*clock() / CLOCKS_PER_SEC << " sec\n";
-	#endif
 }
